1df0eb1196ff2a67ea8624855498e1e77c25d6c4
Famille Brochard <brochard@imac-de-famille.home>
2023-01-12 19:10:32 +0100

    use counter object

--- words_frequency/test_words_frequency.py
+++ words_frequency/test_words_frequency.py
@@ -3,10 +3,6 @@ import pytest
 from .words_frequency import words_frequency
 
 
-def test_words_frequency_raise_value_error():
-    with pytest.raises(ValueError):
-        words_frequency("test toto", -1)
-
 
 def test_words_frequency():
     assert words_frequency("test toto test toto a b", 3) == [
--- words_frequency/words_frequency.py
+++ words_frequency/words_frequency.py
@@ -1,18 +1,14 @@
 from typing import List, Tuple
-
+from collections import Counter
 
 def words_frequency(sentence: str, n: int) -> List[Tuple[str, int]]:
     """
     Function who returns a list of size `n` where each element contains a word and and the frequency of that word in `sentence`
     This list should is sort by decreasing frequency and alphabetical order in case of tie.
     """
-    if n < 0:
-        raise ValueError("n can't be negative")
+
 
     splitted_sentence = sentence.split()
-    unique_words = set(splitted_sentence)
+    splitted_sentence.sort()
+    return Counter(splitted_sentence).most_common(n)
 
-    count = [(word, splitted_sentence.count(word)) for word in unique_words]
-
-    count.sort(key=lambda a: (-a[1], a[0]))
-    return count[0:n]
